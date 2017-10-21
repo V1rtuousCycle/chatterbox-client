3d90c399b7ed80dd8dcac52d9d7998f187ca1849
Anton V Shtylman <AntonVS.Professional@gmail.com>
2017-10-20 17:00:50 -0700

    Attached the POST and GET methods, stubbed out the other required methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,54 @@
+var app = {
+  init: function() {}
+};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: "http://parse.hrr.hackreactor.com/chatterbox/classes/messages",
+    type: "POST",
+    data: message,
+    contentType: "application/json",
+    success: function(data) {
+      console.log("chatterbox: Message sent");
+      console.log(data);
+    },
+    error: function(data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error("chatterbox: Failed to send message", data);
+    }
+  });
+};
+
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    // url: "http://parse.hrr.hackreactor.com/chatterbox/classes/messages",
+    type: "GET",
+    contentType: "application/json",
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error("chatterbox: Failed to send message", data);
+    }
+  });
+};
+
+app.clearMessages = function() {};
+
+app.renderMessage = function(message) {};
+
+app.renderRoom = function() {};
+
+app.renderMessage = function() {};
+
+var message = {
+  username: "shawndrost9",
+  text: "trololo",
+  roomname: "4chan"
+};
+
 // YOUR CODE HERE:
---
+++ format.js
@@ -0,0 +1,20 @@
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
